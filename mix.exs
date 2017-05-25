fc7993dc54988c09daabc01bbe985f54a5dc5129
Muriel Silveira <murielkong@gmail.com>
2017-05-26 00:48:08 +0200

    Add and config the mix_test_watch library to be able to iterate fast when developing and running the init tests

--- mix.exs
+++ mix.exs
@@ -10,24 +10,14 @@ defmodule JsonFeed.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:poison, "~> 3.0"}]
+    [
+      {:poison, "~> 3.0"},
+      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false}
+    ]
   end
 end
