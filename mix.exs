89bc7fddd8157cb586c58a4647e4b7e9348d8acd
Muriel Silveira <murielkong@gmail.com>
2017-05-26 03:15:06 +0200

    Add information to publish the first version on Hex

--- mix.exs
+++ mix.exs
@@ -2,12 +2,18 @@ defmodule JsonFeed.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :json_feed,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :json_feed,
+      version: "0.1.0-alpha",
+      name: "JSON Feed",
+      description: "A JSON Feed implementation in Elixir",
+      source_url: "https://github.com/murielsilveira/json_feed_elixir",
+      package: package(),
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+    ]
   end
 
   def application do
@@ -17,7 +23,18 @@ defmodule JsonFeed.Mixfile do
   defp deps do
     [
       {:poison, "~> 3.0"},
-      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      name: :json_feed,
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Muriel Silveira"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/murielsilveira/json_feed_elixir"}
     ]
   end
 end
